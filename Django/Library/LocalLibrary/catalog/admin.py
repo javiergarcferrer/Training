b33848304e8b3b78328a569c6407c5a15b7a2d2a
javiergarcferrer <javiergarcferrer@gmail.com>
2020-06-16 21:57:42 -0500

    model registering

--- Django/Library/LocalLibrary/catalog/admin.py
+++ Django/Library/LocalLibrary/catalog/admin.py
@@ -4,8 +4,18 @@ from django.contrib import admin
 
 from .models import Author, Genre, Language, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+	list_display = ('book', 'status', 'due_back', 'id')
+	list_filter = ('status', 'due_back')
